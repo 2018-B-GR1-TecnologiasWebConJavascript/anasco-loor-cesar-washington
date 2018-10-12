d799fcb9ed092013d63389294d29aa006d11a55e
Cesar Añasco <wacho_jax@hotmail.com>
2018-10-12 17:44:44 -0500

    If Else

--- 01-JavaScript/01-variables.js
+++ 01-JavaScript/01-variables.js
@@ -14,6 +14,19 @@ var hijos = null;
 
 var dato;
 
+var fechaNacimient = new Date();
+
+var adrian = {
+    "nombre": "Adrian",
+    "segundoNombre": 'Vicente',
+    apellidoPaterno: `Eguez`,
+    edad: 29,
+    hijos: null,
+    mascotas:{
+        nombre: 'Chubaca'
+    }
+}; //object
+
 console.log("HOLA MUNDO");
 
 console.log('edad', typeof edad);               //number
@@ -22,4 +35,68 @@ console.log('sueldo',typeof sueldo);            //number
 console.log('casado',typeof casado);            //boolean
 console.log('hijos', typeof hijos);             //objeto
 console.log('dato', typeof  dato);              //indefinido <- es un tipo de dato
+console.log('fechaNacimiento',typeof  fechaNacimient);
+console.log(adrian.mascotas.nombre);
+console.log(adrian);
+
+//Eliminar propiedad de json
+delete adrian.hijos;
+
+//AAgregar propiedad a json que sea json
+adrian.hija = {
+    nombre: 'Susana'
+};
+console.log(adrian.hija.nombre)
+
+// IF & ELSE
+
+if (true){
+    console.log("Si")
+} else {
+    console.log("No")
+}
+
+if (false){ //Falsy
+    console.log("Si")
+} else {
+    console.log("No")
+}
+
+if (1){ //Thruthy
+    console.log("Si")
+} else {
+    console.log("No")
+}
+
+if (-1){ //Thruthy
+    console.log("Si")
+} else {
+    console.log("No")
+}
+
+if ("s"){ //Thruthy
+    console.log("Si")
+} else {
+    console.log("No")
+}
+
+if (null){ //Falsy
+    console.log("Si")
+} else {
+    console.log("No")
+}
+
+if ({nombre:''}){ //Thruthy
+    console.log("Si")
+} else {
+    console.log("No")
+}
+
+if (undefined){ //Falsy
+    console.log("Si")
+} else {
+    console.log("No")
+}
+
+
 
