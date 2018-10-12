5dcc0c467763b8e547dabbd176b993da19de11c1
Cesar Añasco <wacho_jax@hotmail.com>
2018-10-12 17:11:08 -0500

    Tipos de datos

---
+++ 01-JavaScript/01-variables.js
@@ -0,0 +1,25 @@
+// Entero = int edad = 10;
+
+// Javascript es un lenguaje NO TIPADO
+
+var edad = 10;
+
+var edadstring =  "10";
+
+var sueldo = 1.234;
+
+var casado = false;
+
+var hijos = null;
+
+var dato;
+
+console.log("HOLA MUNDO");
+
+console.log('edad', typeof edad);               //number
+console.log('edadString',typeof edadstring);    //string
+console.log('sueldo',typeof sueldo);            //number
+console.log('casado',typeof casado);            //boolean
+console.log('hijos', typeof hijos);             //objeto
+console.log('dato', typeof  dato);              //indefinido <- es un tipo de dato
+
