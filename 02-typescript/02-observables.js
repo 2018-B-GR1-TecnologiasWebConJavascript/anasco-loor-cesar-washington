0e8c392f583c5c9218bcdf9f05f4beaf4f0606ac
Añasco Cesar <wacho_jax@hotmail.com>
2018-11-09 16:46:46 -0500

    Observables

--- 02-typescript/02-observables.js
+++ 02-typescript/02-observables.js
@@ -1,11 +1,14 @@
 // 01-Observables.ts
 const rxjs = require('rxjs');
 const map = require('rxjs/operators').map;
-const numeros$ = rxjs.of(1, "Adrian", true, { nombre: 'Adrian' }, [1, 2, 3], new Date());
+const distinct = require('rxjs/operators').distinct();
+const numeros$ = rxjs.of(1, "Adrian", 1, true, "Adrian", { nombre: 'Adrian' }, 1, [1, 2, 3], 1, new Date());
 console.log(numeros$);
 numeros$
-    .pipe(map(() => {
-    return false;
+    .pipe(distinct(), map((valorActual) => {
+    return {
+        data: valorActual
+    };
 }))
     .subscribe((ok) => {
     console.log('En ok', ok);
--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -6,14 +6,19 @@
 declare var require: any;
 const rxjs = require('rxjs');
 const map = require('rxjs/operators').map;
+const distinct = require('rxjs/operators').distinct();
 
 
 const numeros$ = rxjs.of(
     1,
     "Adrian",
+    1,
     true,
+    "Adrian",
     {nombre: 'Adrian'},
+    1,
     [1,2,3],
+    1,
     new Date()
 );
 
@@ -23,9 +28,12 @@ console.log(numeros$);
 
 numeros$
     .pipe(
+        distinct(),
         map(
-            ()=> {
-                return false;
+            (valorActual)=> {
+                return {
+                    data: valorActual
+                };
             }
         )
 
@@ -35,7 +43,7 @@ numeros$
         console.log('En ok', ok)
     },
     (error)=>{
-        console.log('Eroor', error);
+        console.log('Error', error);
     },
     ()=>{
         console.log('Completado');
