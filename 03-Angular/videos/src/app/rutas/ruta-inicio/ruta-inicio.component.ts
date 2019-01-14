af6b10fc9f8978321535391ab27ad00121a2c378
wasio1995 <wacho_jax@hotmail.com>
2019-01-14 18:01:02 -0500

    Final clase
    Validacion letra A  y componente web.

--- 03-Angular/videos/src/app/rutas/ruta-inicio/ruta-inicio.component.ts
+++ 03-Angular/videos/src/app/rutas/ruta-inicio/ruta-inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-ruta-inicio',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutaInicioComponent implements OnInit {
 
-  constructor() { }
+  imagenes: Imagenes[] = [
+    {
+      anio: 2018,
+      nombreImagen: '01.jpeg',
+      nombrePelicula: 'uno'
+    },
+    {
+      anio: 2012,
+      nombreImagen: '02.jpeg',
+      nombrePelicula: 'dos'
+    },
+    {
+      anio: 2014,
+      nombreImagen: '03.jpeg',
+      nombrePelicula: 'tres'
+    },{
+      anio: 2010,
+      nombreImagen: '04.jpeg',
+      nombrePelicula: 'cuatro'
+    }
+
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
 }
+
+export interface Imagenes {
+  nombreImagen: string;
+  nombrePelicula: string;
+  anio: number;
+}
+
+
